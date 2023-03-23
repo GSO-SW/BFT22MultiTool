5e65b870cfde58c56426544d4a6332dfb0f389da
Timsolo0408 <112071043+Timsolo0408@users.noreply.github.com>
2023-03-23 11:05:24 +0100

    Update Feature23.cs

--- MABFTKlassenBibliothek/Feature23.cs
+++ MABFTKlassenBibliothek/Feature23.cs
@@ -1,1 +1,50 @@
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MABFTKlassenBibliothek
+{
+    internal class Feature23
+    {
+        internal static void Feature_23()
+        {
+            Console.WriteLine("Test Feature 23");
+            while (true) 
+            {
+                Console.Clear();
+                Console.WriteLine("Wilkommen beim Quadratische Funktion Rechner");
+                Console.WriteLine("Für eine Neue berechnung bitte 1 drücken");
+                Console.WriteLine("Um die voher eingegeben pq daten aufzurufen bitte 2 drücken");
+                Console.WriteLine("Um ins Haubtmenü zurück zu kommen bitte 3 drücken");
+                Console.WriteLine("Um das programm zu beenden Bitte 4 drückern");
+                string Menü = "";
+                Menü = Console.ReadLine();
+                if (Menü == "1") 
+                {
+
+                } 
+                else if (Menü == "2") 
+                {
+
+                } 
+                else if (Menü == "3")
+                {
+
+                }
+                else if (Menü == "4") 
+                {
+                    Environment.Exit(0);
+                }
+
+                //else;
+
+
+
+            }
+
+        }
+    }
+}
