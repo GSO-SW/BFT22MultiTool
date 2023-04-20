eaebc5fd07597ce0fc08b81eca229bcd8a13c663
Dominic Braun <dominic.b7@gso.schule.koeln>
2023-04-20 08:50:47 +0200

    Aktuell

---
+++ ETBFTKlassenBibliothek/Feature4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETBFTKlassenBibliothek
+{
+    internal class Feature4
+    {
+        internal static void Feature_4()
+        {
+            Console.WriteLine("Test Feature 4");
+            //Diese Ausgabe hilft Ihnen zu erkennen ob der Aufruf funktioniert.
+        }
+
+    }
+}
