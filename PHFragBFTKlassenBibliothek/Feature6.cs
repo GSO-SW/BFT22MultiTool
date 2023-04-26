764fc8d1ab9ea655f5dac3f61ff843bc6eaddc90
Lukas Zangen <lukas.z18@gso.schule.koeln>
2023-04-26 23:49:16 +0200

    Programm verschönert.

--- PHFragBFTKlassenBibliothek/Feature6.cs
+++ PHFragBFTKlassenBibliothek/Feature6.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,47 +11,193 @@ namespace PHFragBFTKlassenBibliothek
     {
         internal static void feature_6()
         {
-            Console.WriteLine("------------------------------------------------------------------------------------\n" +
-                              "|                 >>> Willkommen beim Physik Konverter <<<                         |\n" +
-                              "------------------------------------------------------------------------------------\n\n");
-            Console.WriteLine("Wählen Sie eine Option:");
-            Console.WriteLine("1 - Meter in Kilometer umrechnen");
-            Console.WriteLine("2 - Zentimeter in Meter umrechnen");
-            Console.WriteLine("3 - Gramm in Kilogramm umrechnen");
-            Console.WriteLine("4 - Millisekunden in Sekunden umrechnen\n");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("+------------------------------------------------+\n" +
+                              "|                                                |\n" +
+                              "|        Willkommen beim Physik Konverter        |\n" +
+                              "|                                                |\n" +
+                              "+------------------------------------------------+\n" +
+                              "|                                                |\n" +
+                              "|     Eingabe: exit  ->  beendet das Programm    |\n" +
+                              "|                                                |\n" +
+                              "|     1. Meter in Kilometer umrechnen            |\n" +
+                              "|     2. Zentimeter in Meter umrechnen           |\n" +
+                              "|     3. Gramm in Kilogramm umrechnen            |\n" +
+                              "|     4. Millisekunden in Sekunden umrechnen     |\n" +
+                              "|                                                |\n" +
+                              "+------------------------------------------------+\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("Bitte wählen sie eine Option: ");
-            int option = int.Parse(Console.ReadLine());
+            Console.ResetColor();
+            string option = Console.ReadLine();
 
             switch (option)
             {
-                case 1:
+                case "1":
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "|         Meter in Kilometer umrechnen         |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "| Info:                                        |\n" +
+                                      "|                                              |\n" +
+                                      "| Die Einheit Kilometer gehört zum metrischen  |\n" +
+                                      "| System und leitet sich von der Basiseinheit  |\n" +
+                                      "| Meter ab. Dabei entspricht 1 Kilometer       |\n" +
+                                      "| 1.000 Metern bzw. 1 Meter entspricht 0,001   |\n" +
+                                      "| Kilometer.                                   |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.Write("Bitte geben Sie die Länge in Metern ein: ");
-                    double meters = double.Parse(Console.ReadLine());
-                    double kilometers = meters / 1000;
-                    Console.WriteLine("{0} Meter entsprechen {1} Kilometer.", meters, kilometers);
+                    Console.ResetColor();
+                    string input = Console.ReadLine();
+                    if (input.ToLower() == "exit")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (input.ToLower() == "subexit")
+                    {
+                        return;
+                    }
+                    else if (double.TryParse(input, out double meters))
+                    {
+                        double kilometers = meters / 1000;
+                        Console.WriteLine($"{meters} Meter entsprechen {kilometers} Kilometer.");
+                    }   
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein");
+                        Console.ResetColor();
+                    }
                     break;
-
-                case 2:
+                case "2":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "|        Zentimeter in Meter umrechnen         |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "| Info:                                        |\n" +
+                                      "|                                              |\n" +
+                                      "| Die Einheit Kilometer gehört zum metrischen  |\n" +
+                                      "| System und leitet sich von der Basiseinheit  |\n" +
+                                      "| Meter ab. Dabei entspricht 1 Kilometer       |\n" +
+                                      "| 1.000 Metern bzw. 1 Meter entspricht 0,001   |\n" +
+                                      "| Kilometer.                                   |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.Write("Bitte geben Sie die Länge in Zentimetern ein: ");
-                    double centimeters = double.Parse(Console.ReadLine());
-                    double meters2 = centimeters / 100;
-                    Console.WriteLine("{0} Zentimeter entsprechen {1} Metern.", centimeters, meters2);
+                    Console.ResetColor();
+                    string input1 = Console.ReadLine();
+                    if (input1.ToLower() == "exit")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (input1.ToLower() == "subexit")
+                    {
+                        return;
+                    }
+                    else if (double.TryParse(input1, out double centimeters))
+                    {
+                        double meters2 = centimeters / 100;
+                        Console.WriteLine($"{centimeters} Zentimeter entsprechen {meters2} Metern.");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein");
+                        Console.ResetColor();
+                    }
                     break;
-
-                case 3:
+                case "3":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "|          Gram in Kilogram umrechnen          |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "| Info:                                        |\n" +
+                                      "|                                              |\n" +
+                                      "| Die Einheit Kilometer gehört zum metrischen  |\n" +
+                                      "| System und leitet sich von der Basiseinheit  |\n" +
+                                      "| Meter ab. Dabei entspricht 1 Kilometer       |\n" +
+                                      "| 1.000 Metern bzw. 1 Meter entspricht 0,001   |\n" +
+                                      "| Kilometer.                                   |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.Write("Bitte geben Sie das Gewicht in Gramm ein: ");
-                    double grams = double.Parse(Console.ReadLine());
-                    double kilograms = grams / 1000;
-                    Console.WriteLine("{0} Gramm entsprechen {1} Kilogramm.", grams, kilograms);
+                    Console.ResetColor();
+                    string input2 = Console.ReadLine();
+                    if (input2.ToLower() == "exit")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (input2.ToLower() == "subexit")
+                    {
+                        return;
+                    }
+                    else if (double.TryParse(input2, out double grams))
+                    {
+                        double kilograms = grams / 1000;
+                        Console.WriteLine($"{grams} Gramm entsprechen {kilograms} Kilogramm.", grams, kilograms);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein");
+                        Console.ResetColor();
+                    }
                     break;
-
-                case 4:
+                case "4":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "|     Millisekunden in Sekunden umrechnen      |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n" +
+                                      "|                                              |\n" +
+                                      "| Info:                                        |\n" +
+                                      "|                                              |\n" +
+                                      "| Die Einheit Kilometer gehört zum metrischen  |\n" +
+                                      "| System und leitet sich von der Basiseinheit  |\n" +
+                                      "| Meter ab. Dabei entspricht 1 Kilometer       |\n" +
+                                      "| 1.000 Metern bzw. 1 Meter entspricht 0,001   |\n" +
+                                      "| Kilometer.                                   |\n" +
+                                      "|                                              |\n" +
+                                      "+----------------------------------------------+\n");
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
                     Console.Write("Bitte geben Sie die Zeit in Millisekunden ein: ");
-                    double milliseconds = double.Parse(Console.ReadLine());
-                    double seconds = milliseconds / 1000;
-                    Console.WriteLine("{0} Millisekunden entsprechen {1} Sekunden.", milliseconds, seconds);
+                    Console.ResetColor();
+                    string input3 = Console.ReadLine();
+                    if (input3.ToLower() == "exit")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (input3.ToLower() == "subexit")
+                    {
+                        return;
+                    }
+                    else if (double.TryParse(input3, out double milliseconds))
+                    {
+                        double seconds = milliseconds / 1000;
+                        Console.WriteLine("{0} Millisekunden entsprechen {1} Sekunden.", milliseconds, seconds);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein");
+                        Console.ResetColor();
+                    }
                     break;
-
                 default:
                     Console.WriteLine("Ungültige Option ausgewählt.");
                     break;
