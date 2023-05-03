11ee0fa6a8d850d0cb469b09860bd3c980f7f940
hasan.y15 <hasan.y15@gso.schule.koeln>
2023-05-03 15:02:44 +0200

    feature19 neu

--- WIBFTKlassenBibliothek/WIMenue.cs
+++ WIBFTKlassenBibliothek/WIMenue.cs
@@ -50,7 +50,7 @@ namespace WIBFTKlassenBibliothek
                 {
                     case "e":
                         Console.Clear();
-                        Handyvertrag.Handyvertrag1();
+                        Feature19.Feature_19();
                         break;
 
                     case "i":
